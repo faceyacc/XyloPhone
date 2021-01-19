953e03491cc91814d528fdbcbb797a6ac23dd350
Ty Facey <justfacey@gmail.com>
2021-01-19 00:58:03 -0500

    Added all seven xylophone notes to UI

--- xylophone_flutterapp/lib/main.dart
+++ xylophone_flutterapp/lib/main.dart
@@ -11,16 +11,62 @@ class XyloPhone extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.yellow,
+          title: Text('XyloPhone App'),
+          backgroundColor: Colors.black,
         ),
         body: SafeArea(
           child: Center(
-            child: FlatButton(
-              onPressed: () {
-                AudioCache().play('note3.wav');
-              },
-              child: Text('hej'),
-            ),
+            child: Column(children: <Widget>[
+              FlatButton(
+                color: Colors.red,
+                onPressed: () {
+                  AudioCache().play('note1.wav');
+                },
+                child: null,
+              ),
+              FlatButton(
+                color: Colors.orange,
+                onPressed: () {
+                  AudioCache().play('note2.wav');
+                },
+                child: null,
+              ),
+              FlatButton(
+                color: Colors.yellow,
+                onPressed: () {
+                  AudioCache().play('note3.wav');
+                },
+                child: null,
+              ),
+              FlatButton(
+                color: Colors.green,
+                onPressed: () {
+                  AudioCache().play('note4.wav');
+                },
+                child: null,
+              ),
+              FlatButton(
+                color: Colors.green[900],
+                onPressed: () {
+                  AudioCache().play('note5.wav');
+                },
+                child: null,
+              ),
+              FlatButton(
+                color: Colors.lightBlue,
+                onPressed: () {
+                  AudioCache().play('note6.wav');
+                },
+                child: null,
+              ),
+              FlatButton(
+                color: Colors.purple,
+                onPressed: () {
+                  AudioCache().play('note7.wav');
+                },
+                child: null,
+              ),
+            ]),
           ),
         ),
       ),
